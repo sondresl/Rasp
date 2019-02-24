68945e5d60ad85d2cd19d7ea1b668af30118281e
gaute <gaute.berge@gmail.com>
2019-02-25 00:05:42 +0100

    deleted old error struct

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -1,22 +1,6 @@
 use crate::scanner::token::Token;
 use std::io;
 
-//#[derive(Debug,new)]
-//pub struct ParseError {
-//    pub expected: Token,
-//    pub found: Token,
-//    pub line_number: u32
-//}
-//
-//impl Display for ParseError {
-//    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//        write!(f, "Expected: <{:?}>, but found <{:?}> on line: {}",
-//               self.expected,
-//               self.found,
-//               self.line_number)
-//    }
-//}
-
 #[derive(Debug,Fail)]
 pub enum ParseError {
     #[fail(display = "Expected <{:#?}>, but found <{:#?}> on line {}", expected, found, line_number)]
